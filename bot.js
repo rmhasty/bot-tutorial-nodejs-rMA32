590276a95ed34a8f0a0d3fefc4000f960c0595af
rmhasty <rhetthasty@yahoo.com>
2017-01-15 15:38:39 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ var HTTPS = require('https');
 	      botRegexSiege = /^\/siege/; botRegexOW = /^\/ratings/; 
 	      botRegexSlut = /^\/help/; botRegexStop = /^\/fb/;
 	      botRegexProp = /^\/prop/;botRegexKys = /^\/kys/; botRegexSlam = /^\/slam/; botRegexBrye = /^\/brye/;
-	      botRegexGian = /^\/gian/; botRegexWelcome = /^\/welcome/; botRegexShaki = /^\/shaki/;
+	      botRegexNice = /^\/nice/; botRegexWelcome = /^\/welcome/; botRegexShaki = /^\/shaki/;
 	      botRegexDaf = /^\/dafuq/; botRegexAyy = /^\/ayy/; botRegexSchedule = /^\/schedule/;
 	      siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
 	      siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
@@ -55,9 +55,9 @@ var HTTPS = require('https');
 	    postMessage("https://i.groupme.com/600x490.jpeg.b31d5153fb474d9bb18dd51b3d650afe.large");
 	    this.res.end();
 	  } 
-	  else if(request.text && botRegexGian.test(request.text)) {
+	  else if(request.text && botRegexNice.test(request.text)) {
 	    this.res.writeHead(200);
-	    postMessage("http://i.imgur.com/9V6ZnvD.png");
+	    postMessage("http://giphy.com/gifs/south-park-R8oQcpddGK92g");
 	    this.res.end();
 	  } 
 	  else if(request.text && botRegexWelcome.test(request.text)) {
